738688abcc06cc6f38716843881e8162ed510ad2
Valerii <vd.alternative@gmail.com>
2023-11-06 23:35:07 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,3 +1,52 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  btnStart: document.querySelector('button[data-start]'),
+  days: document.querySelector('.value[data-days]'),
+  hours: document.querySelector('.value[data-hours]'),
+  minutes: document.querySelector('.value[data-minutes]'),
+  seconds: document.querySelector('.value[data-seconds]'),
+};
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const datePicked = selectedDates[0];
+    const dateNow = new Date();
+    console.log(selectedDates[0]);
+  },
+};
+const datePicker = flatpickr('#datetime-picker', options);
+
+refs.btnStart.addEventListener('click', onBtnClickHendler);
+function onBtnClickHendler() {
+  onBtnStartDisable();
+}
+
+function onBtnStartDisable() {
+  refs.btnStart.disabled = true;
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
