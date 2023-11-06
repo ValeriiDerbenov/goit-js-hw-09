f4fb11dfdf9ddfb6baacda522d2b27ce079aaca4
ValeriiDerbenov <129115422+ValeriiDerbenov@users.noreply.github.com>
2023-11-06 11:47:12 +0000

    Deploying to gh-pages from @ ValeriiDerbenov/goit-js-hw-09@84f1c01bcdfddb3f063d75221d5b4e604992ab23 🚀

---
+++ 01-color-switcher.50a91e8f.js
@@ -0,0 +1,2 @@
+const t={body:document.querySelector("body"),buttonStart:document.querySelector("button[data-start]"),buttonStop:document.querySelector("button[data-stop]")};t.buttonStart.addEventListener("click",onStartHendler),t.buttonStart.addEventListener("click",onStopHendler);
+//# sourceMappingURL=01-color-switcher.50a91e8f.js.map
---
+++ 01-color-switcher.e6ef39c0.js
@@ -0,0 +1,2 @@
+!function(){var t={body:document.querySelector("body"),buttonStart:document.querySelector("button[data-start]"),buttonStop:document.querySelector("button[data-stop]")};t.buttonStart.addEventListener("click",onStartHendler),t.buttonStart.addEventListener("click",onStopHendler)}();
+//# sourceMappingURL=01-color-switcher.e6ef39c0.js.map
