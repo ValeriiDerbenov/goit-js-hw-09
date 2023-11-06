84f1c01bcdfddb3f063d75221d5b4e604992ab23
Valerii <vd.alternative@gmail.com>
2023-11-06 13:46:51 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,15 @@
+const refs = {
+  body: document.querySelector('body'),
+  buttonStart: document.querySelector('button[data-start]'),
+  buttonStop: document.querySelector('button[data-stop]'),
+};
+let intervalId = null;
 
+refs.buttonStart.addEventListener('click', onStartHendler);
+refs.buttonStart.addEventListener('click', onStopHendler);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
